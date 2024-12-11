c0765625737fe5022c18a86a1208c34e5165bd37
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-11 20:52:01 +0700

    page dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+
+const DashboardPage = () => {
+  return (
+    <div className="container mx-auto mt-16 text-center">
+      <h1 className="text-2xl font-bold">
+        Selamat datang kembali di dashboard admin
+      </h1>
+    </div>
+  );
+};
+
+export default DashboardPage;
